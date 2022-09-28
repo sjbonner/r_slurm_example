4defc54ec13702bab2c289e653e7b0936db140d4
Simon Bonner <sbonner6@uwo.ca>
2022-09-28 11:46:03 -0400

    Removed unnecessary argument to parameter matrix file.

--- slurm_example.sh
+++ slurm_example.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --array=1-10
+#SBATCH --array=1-50
 #SBATCH --time=00:05:00
 #SBATCH --mem=1G
 #SBATCH --error=Logs/rep_%A_%a.log
@@ -7,4 +7,4 @@
 
 module load StdEnv/2020  gcc/9.3.0  r/4.1.2
 
-Rscript --vanilla slurm_example.R $SLURM_ARRAY_TASK_ID parameter_matrix.csv &> Logs/rep_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.log
+Rscript --vanilla slurm_example.R $SLURM_ARRAY_TASK_ID &> Logs/rep_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.log
