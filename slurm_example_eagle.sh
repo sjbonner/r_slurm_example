d44163510ea0910176b5641eb536f95dcf957a2d
Simon Bonner <sbonner6@eagle.stats.uwo.ca>
2023-06-28 12:40:45 -0400

    Removed modules line from slurm_example_eagle.sh.

--- slurm_example_eagle.sh
+++ slurm_example_eagle.sh
@@ -5,6 +5,4 @@
 #SBATCH --error=Logs/rep_%A_%a.log
 #SBATCH --output=Logs/rep_%A_%a.log
 
-module load StdEnv/2020  gcc/9.3.0  r/4.1.2
-
 Rscript --vanilla slurm_example.R $SLURM_ARRAY_TASK_ID &> Logs/rep_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.log
